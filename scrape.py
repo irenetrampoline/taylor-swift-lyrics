ba89960e9b609753aeeb574cf76aa443c9f65e6b
Irene Chen <irene@dropbox.com>
2016-01-26 11:00:31 -0800

    add first scrape round

--- scrape.py
+++ scrape.py
@@ -1,8 +1,13 @@
 import bs4
 import urllib
 from urlparse import urljoin
+import gzip
+import json
+from os.path import join
 
-AZLYRICS_URL = 'http://www.azlyrics.com/lyrics/taylorswift/timmcgraw.html'
+import py.test
+
+AZLYRICS_URL = 'http://www.azlyrics.com/t/taylorswift.html'
 METROLYRICS_URL = 'http://www.metrolyrics.com/taylor-swift-lyrics.html'
 
 EX_URL = 'http://www.azlyrics.com/lyrics/taylorswift/aplaceinthisworld.html'
@@ -13,37 +18,53 @@ class LyricsWalker(object):
 	Class for lyric gathering. Currently for AZ Lyrics, but can be adapted into
 	base case.
 	"""
-	def __init__(self, home_url):
-		self.home_url = home_url
-
-	@staticmethod
-	def get_lyrics(url=EX_URL, album=False):
+	@classmethod
+	def get_song_info(cls, url=EX_URL):
 		"""
 		Grabs lyrics from URL and return plain text lyrics.
 		Lyrics are located at /html/body/div[3]/div/div[2]/div[6]
 		"""
-		soup = get_soup_from_url(url)
+		soup = cls.get_soup_from_url(url)
 
-		lyrics = soup.html.body.findAll('div')[10].div.findAll('div')[2].findAll('div')[7].text.strip()
-		print 'Parsed %s' % soup.title.text
+		try:
+			album_info = soup.find('div', {'class': 'album-panel'}).text.strip()
+			album = album_info.split('"')[1]
+			year = album_info.split('(')[1][:-1]
+			title = soup.title.text.split(' - ')[-1]
+			lyrics = soup.html.body.findAll('div')[10].div.findAll('div')[2].findAll('div')[7].text.strip()
+		except:
+			py.test.set_trace()
 
-		if album:
-			return lyrics, None
+		entry_info = {
+			'lyrics': lyrics,
+			'title': album,
+			'year': year,
+			'title': title
+		}
 
-		return lyrics
+		print 'Parsed %s' % title
+		return entry_info
 
-
-	@staticmethod
-	def get_soup_from_url(url):
+	@classmethod
+	def get_soup_from_url(cls, url):
 		urlobject = urllib.urlopen(url)
 		return bs4.BeautifulSoup(urlobject.read())
 
-	def walk_homepage(self):
-		urlobject = get_soup_from_url(self.home_url)
+	@classmethod
+	def walk_homepage(cls, home_url, output_dir=''):
+		soup = cls.get_soup_from_url(home_url)
+		py.test.set_trace()
 		song_lst = soup.find(id='listAlbum').findAll(target='_blank')
 
-		for song in song_lst:
-			song_url = urljoin(self.homeurl, song.get('href'))
-			get_lyrics(song_url)
+		had_previous = False
+		filename = join(output_dir, 'az_lyrics.json.gz')
+		with open(filename, 'wb') as f:
+			for song in song_lst:
+				f.write(',' if had_previous else '[')
+				song_url = urljoin(home_url, song.get('href'))
+				json.dump(cls.get_song_info(song_url), f)
+			f.write(']')
+
 if __name__ == '__main__':
-	print parse_entry(EX_URL)
+	py.test.set_trace()
+	LyricsWalker.walk_homepage(AZLYRICS_URL, '')
